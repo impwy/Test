e8cd1878992e0a11a279a8d3d3e1536641d26945
yong <pwy6817@gmail.com>
2023-03-13 19:32:44 +0900

    Member

---
+++ land/src/main/java/com/land/myapp/user/MemberVO.java
@@ -0,0 +1,119 @@
+package com.land.myapp.user;
+
+import java.sql.Date;
+
+public class MemberVO {
+    private int user_no;
+    private String user_id;
+    private String user_pwd;
+    private String user_name;
+    private String user_phone;
+    private int user_age;
+    private String user_email;
+    private String user_addr;
+
+    //주민등록번호
+    private String user_key;
+    private Date user_date;
+
+    //등급을 숫자로
+    private int user_grade;
+
+    private  int user_discount;
+
+    public int getUser_no() {
+        return user_no;
+    }
+
+    public void setUser_no(int user_no) {
+        this.user_no = user_no;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUser_pwd() {
+        return user_pwd;
+    }
+
+    public void setUser_pwd(String user_pwd) {
+        this.user_pwd = user_pwd;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getUser_phone() {
+        return user_phone;
+    }
+
+    public void setUser_phone(String user_phone) {
+        this.user_phone = user_phone;
+    }
+
+    public int getUser_age() {
+        return user_age;
+    }
+
+    public void setUser_age(int user_age) {
+        this.user_age = user_age;
+    }
+
+    public String getUser_email() {
+        return user_email;
+    }
+
+    public void setUser_email(String user_email) {
+        this.user_email = user_email;
+    }
+
+    public String getUser_addr() {
+        return user_addr;
+    }
+
+    public void setUser_addr(String user_addr) {
+        this.user_addr = user_addr;
+    }
+
+    public String getUser_key() {
+        return user_key;
+    }
+
+    public void setUser_key(String user_key) {
+        this.user_key = user_key;
+    }
+
+    public Date getUser_date() {
+        return user_date;
+    }
+
+    public void setUser_date(Date user_date) {
+        this.user_date = user_date;
+    }
+
+    public int getUser_grade() {
+        return user_grade;
+    }
+
+    public void setUser_grade(int user_grade) {
+        this.user_grade = user_grade;
+    }
+
+    public int getUser_discount() {
+        return user_discount;
+    }
+
+    public void setUser_discount(int user_discount) {
+        this.user_discount = user_discount;
+    }
+}
